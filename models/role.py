8a41b0eb2ea080375ff8445975fb4ef8529b655b
watanabeyu1218 <k23145kk@aitech.ac.jp>
2024-12-07 16:08:42 +0900

    飼育表画面修正
    order -> role
    product -> animal
    user -> keeper

--- models/role.py
+++ models/role.py
@@ -6,7 +6,7 @@ from .product import Product
 class Role(Model):
     keeper = ForeignKeyField(User, backref='keepers')#飼育員の名前
     animalname = ForeignKeyField(Product, backref='animal_names')#動物の愛称
-    order_date = DateTimeField()#そのまま
+    role_date = DateTimeField()
 
     class Meta:
         database = db
