f9bc3b78b5784e5b989bda10383b9aa5f4c68de0
Miyagoshi_Sota <k23122kk@aitech.ac.jp>
2024-12-19 10:01:15 +0900

    fix:change visitor graph Ybar beginAtZero

--- routes/visitor.py
+++ routes/visitor.py
@@ -10,17 +10,21 @@ visitor_bp = Blueprint('visitor', __name__, url_prefix='/visitors')
 
 
 def dummy():
-    for y in range(2020,2025):
-        for m in range(1,13):
-            days =  calendar.monthrange(y, m)[1]
-            print("{} {} {}".format(y,m,days))
-            for d in range(1,days+1):
-                Visitor.create(date=date(y,m,d), male=random.randrange(300,1500,1), female=random.randrange(300,1500,1), boy=random.randrange(300,1500,1), girl=random.randrange(300,1500,1))
+    for y in range(2020, 2025):
+        for m in range(1, 13):
+            days = calendar.monthrange(y, m)[1]
+            print("{} {} {}".format(y, m, days))
+            for d in range(1, days + 1):
+                Visitor.create(date=date(y, m, d),
+                               male=random.randrange(300, 1500, 1),
+                               female=random.randrange(300, 1500, 1),
+                               boy=random.randrange(300, 1500, 1),
+                               girl=random.randrange(300, 1500, 1))
 
 
 @visitor_bp.route('/data')
 def list():
-    #dummy()
+    dummy()
     # データ取得
     visitors = Visitor.select()
     print(type(visitors))
@@ -30,21 +34,28 @@ def list():
         y = visitor.date.year
         m = visitor.date.month
         d = visitor.date.day
-        data.append({"{}-{}-{}".format(y, m, d):{"male":visitor.male, "female":visitor.female, "boy":visitor.boy, "girl":visitor.girl}})
+        data.append({
+            "{}-{}-{}".format(y, m, d): {
+                "male": visitor.male,
+                "female": visitor.female,
+                "boy": visitor.boy,
+                "girl": visitor.girl
+            }
+        })
 
     monthly_data = {}
     previous_date = date(1, 1, 1)
     current_date = date(1, 1, 1)
     previous_y_m = date(1, 1, 1)
     current_y_m = date(1, 1, 1)
-    data_in_month = {"m":0, "f":0, "b":0, "g":0}
+    data_in_month = {"m": 0, "f": 0, "b": 0, "g": 0}
     for dat in data:
         _date = None
         for day in dat.keys():
             y = int(day.split('-')[0])
             m = int(day.split('-')[1])
             d = int(day.split('-')[2])
-            _date = date(y,m,d)
+            _date = date(y, m, d)
         if _date != None:
             current_date = _date
         if current_date.year != previous_date.year:
@@ -53,19 +64,19 @@ def list():
         if current_date.month != previous_date.month or current_date.year != previous_date.year:
             key = f"{current_y_m.year}-{current_y_m.month}"
             adding_data = {
-                "m":data_in_month['m'],
-                "f":data_in_month['f'],
-                "b":data_in_month['b'],
-                "g":data_in_month['g']
-                }
+                "m": data_in_month['m'],
+                "f": data_in_month['f'],
+                "b": data_in_month['b'],
+                "g": data_in_month['g']
+            }
             #print(f"sum {key} {adding_data}")
-            monthly_data[key]=adding_data
+            monthly_data[key] = adding_data
             print("====================================")
             print("------------------------------------")
             print(monthly_data)
             print("------------------------------------")
             print("====================================")
-            data_in_month = {"m":0, "f":0, "b":0, "g":0}
+            data_in_month = {"m": 0, "f": 0, "b": 0, "g": 0}
             current_y_m = date(current_date.year, current_date.month, 1)
             print(f"change month to {current_date.month}")
             print()
@@ -83,6 +94,5 @@ def list():
     print(monthly_data)
     print("==============")
     print(json.dumps(monthly_data))
-
 
     return json.dumps(monthly_data)
